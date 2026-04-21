05162651f0ddb27367e9af4d6598cd7bc9ee1a4f
angryfloofers-ui <angryfloofers@gmail.com>
2026-04-21 16:45:23 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const ITEMS = [
+  {
+    id: 1,
+    title: "Item One",
+    description: "Description of item one.",
+    price: "0.5 XMR",
+    image: "https://via.placeholder.com/200",
+    moneroAddress: "YOUR_MONERO_ADDRESS_HERE"
+  },
+  {
+    id: 2,
+    title: "Item Two",
+    description: "Description of item two.",
+    price: "1.2 XMR",
+    image: "https://via.placeholder.com/200",
+    moneroAddress: "YOUR_MONERO_ADDRESS_HERE"
+  }
+];
